08612373faf0c58c9a0199d143b6e2759dac1cf9
Jorge Perez <jpr6at@outlook.com>
2021-05-10 23:10:13 -0500

    Interfaces InitializingBean y DisposableBean

--- src/main/java/com/devs4j/di/Dev4jApplication.java
+++ src/main/java/com/devs4j/di/Dev4jApplication.java
@@ -1,13 +1,37 @@
 package com.devs4j.di;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.devs4j.di.lifeCycle.ExplicitBean;
+import com.devs4j.di.lifeCycle.LifeCycleBean;
+
+import ch.qos.logback.core.spi.LifeCycle;
 
 @SpringBootApplication
 public class Dev4jApplication {
+	
+	
+	private static final Logger log = LoggerFactory.getLogger(Dev4jApplication.class);
 
+	
+	@Bean
+	public String getApplicationName() {
+		return ":Devs4j rules";
+	}
+	/*
+	@Bean(initMethod = "init", destroyMethod = "destroy")
+	public ExplicitBean getBean() {
+		return new ExplicitBean();
+	}
+	*/
 	public static void main(String[] args) {
-		SpringApplication.run(Dev4jApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Dev4jApplication.class, args);
+		LifeCycleBean bean = context.getBean(LifeCycleBean.class);
 	}
 
 }
---
+++ src/main/java/com/devs4j/di/lifeCycle/LifeCycleBean.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package com.devs4j.di.lifeCycle;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author jpr6a
+ *
+ */
+
+@Component
+public class LifeCycleBean implements BeanNameAware, InitializingBean, DisposableBean{
+	
+	
+	private static final Logger log = LoggerFactory.getLogger(LifeCycleBean.class);
+	
+	/*
+	 * Durante la construccion de bean "BeanNameAware"
+	 */
+	
+	@Override
+	public void setBeanName(String name) {
+		log.info("Bean name {}",name);
+	}
+	
+	/**
+	 * Esto se ejecutara despues de la carga de la dependencia.
+	 */
+	
+	@PostConstruct
+	public void init() {
+		log.info("Post construct");
+	}
+	
+	/**
+	 * Esto se ejecutara antes de que el bean sea destruido
+	 * No se ejecutan en beans prototype
+	 * Solo se ejecutan durante una terminación de la VM de forma normal
+	 */
+	@PreDestroy
+	public void destroyBean() {
+		log.info("Pre destroy");
+	}
+ 
+	
+	@Override
+	public void destroy() throws Exception{
+		log.info("Destroy method");
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		log.info("After properties set method");
+		
+	}
+	
+}
